982664b334b3a789cf4001d1c9397e4e199b79ae
richp1982 <100537747+richp1982@users.noreply.github.com>
2022-03-03 09:22:58 +0100

    Create bash-setup.sh

---
+++ bash-setup.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+git clone git://github.com/ohmybash/oh-my-bash.git /home/rich/.oh-my-bash
+git clone git://github.com/richp1982/dot-files-arch.git
+cp /a/dot-files/bashrc /home/rich/.bashrc
