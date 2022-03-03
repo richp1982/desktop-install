d597d8ac655ca1ad4dcab5073b72fb4abb41fd88
richp1982 <100537747+richp1982@users.noreply.github.com>
2022-03-03 08:46:04 +0100

    Update install-plasma.sh

--- install-plasma.sh
+++ install-plasma.sh
@@ -49,5 +49,5 @@ cd /a
 git clone https://github.com/gustawho/grub2-theme-breeze.git
 cd grub2-theme-breeze
 sudo cp -r breeze /usr/share/grub/themes/
-sudo echo 'GRUB_THEME="/usr/share/grub/themes/breeze/theme.txt'
+sudo sed -i 's|GRUB_THEME=|GRUB_THEME=/usr/share/grub/themes/breeze/theme.txt"|' /etc/default/grub
 sudo grub-mkconfig -o /boot/grub/grub.cfg
