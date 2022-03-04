defab916d59adce9149f912d254507cbea51d262
richp1982 <100537747+richp1982@users.noreply.github.com>
2022-03-04 16:19:19 +0100

    Delete grub-theme.sh

--- grub-theme.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-cd /a
-git clone https://github.com/gustawho/grub2-theme-breeze.git
-cd grub2-theme-breeze
-sudo cp -r breeze /usr/share/grub/themes/
-sudo sed -i 's|#GRUB_THEME=.*|GRUB_THEME="/usr/share/grub/themes/breeze/theme.txt"|' /etc/default/grub
-sudo grub-mkconfig -o /boot/grub/grub.cfg
