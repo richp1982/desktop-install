e3a1c6ba0fea5624b1a9684f253d3959fa762d25
richp1982 <100537747+richp1982@users.noreply.github.com>
2022-03-05 08:25:18 +0100

    Create clean-up.sh

---
+++ clean-up.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+#set permissions
+sudo chown -R rich:users /data
+sudo chown -R rich:users /data/shared
+sudo chown -R rich:rich /data/users/rich
+sudo chown -R edyta:edyta /data/users/edyta
+sudo chown -R kiwi:kiwi /data/users/kiwi
