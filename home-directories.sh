55d8cb0428f1ea56044a0a74e227dd8252de0a36
richp1982 <100537747+richp1982@users.noreply.github.com>
2022-03-05 08:34:08 +0100

    Update home-directories.sh

--- home-directories.sh
+++ home-directories.sh
@@ -3,9 +3,9 @@
 
 
 #home directories
-sudo rm -r /home/edyta/*
+sudo rm -r /home/edyta
 sudo rm -r /home/rich/*
-sudo rm -r /home/kiwi/*
+sudo rm -r /home/kiwi
 sudo mkdir /home/{edyta,kiwi}
 sudo ln -s /data/shared/* /home/rich/
 sudo ln -s /data/shared/* /home/edyta/
@@ -13,13 +13,3 @@ sudo ln -s /data/shared/* /home/kiwi/
 sudo ln -s /data/users/rich/* /home/rich/
 sudo ln -s /data/users/edyta/* /home/edyta/
 sudo ln -s /data/users/kiwi/* /home/kiwi/
-
-#permissions
-sudo chown rich:rich /home/rich
-sudo chown edyta:edyta /home/edyta
-sudo chown kiwi:kiwi /home/kiwi
-sudo chown -R rich:users /data/shared
-sudo chmod -R g+rwxs /data/shared
-sudo chown -R rich:rich /data/users/rich
-sudo chown -R edyta:edyta /data/users/edyta
-sudo chown -R kiwi:kiwi /data/users/kiwi
