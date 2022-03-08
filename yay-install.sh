ec71c83cc07e1593dc975855684415d2c0108424
richp1982 <100537747+richp1982@users.noreply.github.com>
2022-03-08 09:16:06 +0100

    Update yay-install.sh

--- yay-install.sh
+++ yay-install.sh
@@ -4,3 +4,10 @@ cd /a
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
+
+#install aur packages
+yay -S brave-bin
+yay -S zoom
+yay -S skypeforlinux-stable-bin
+yay -S google-chrome
+yay -S spotify
