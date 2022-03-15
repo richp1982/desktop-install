f1a36976321be96f13c9ebb264075234bb7ad522
richp1982 <100537747+richp1982@users.noreply.github.com>
2022-03-15 08:46:21 +0100

    Update i3.sh

--- i3.sh
+++ i3.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 #install desktop packages
-sudo pacman -Syyu xorg sddm i3 i3-gaps i3lock i3status  --noconfirm --needed
+sudo pacman -Syyu xorg lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings i3 i3-gaps i3lock i3status materia-gtk-theme papirus-icon-theme xfce4-terminal  --noconfirm --needed
 
 #install optional packages
-sudo pacman -S ibus packagekit-qt5 okular kate konsole gparted xf86-input-synaptics ffmpegthumbs ttf-dejavu ttf-liberation dolphin kde-cli-tools kde-graphical-thumbnailers dolphin-plugins dmenu nitrogen picom lxappearance kvantum qt5ct --noconfirm --needed
+sudo pacman -S ibus okular gparted xf86-input-synaptics ffmpegthumbs nemo ttf-dejavu ttf-liberation dmenu nitrogen picom lxappearance archlinux-wallparper --noconfirm --needed
 
 #enable sddm
-sudo systemctl enable sddm
+sudo systemctl enable lightdm
 
-#sddm login settings
-#select keyboard en=english (QWERTY) fr=french (AZERTY)
-echo setxkbmap "?" | sudo tee -a /usr/share/sddm/scripts/Xsetup
-sudo cp sddm.conf /etc/sddm.conf
+#enable numlock on boot
+sudo pacman -S numlockx
+echo "greeter-setup-script=/usr/bin/numlockx on" | tee -a /etc/lightdm/lightdm.conf
+
