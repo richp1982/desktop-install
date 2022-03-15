346daefd221a2cc09adb6fc9c5e442caa629a112
richp1982 <100537747+richp1982@users.noreply.github.com>
2022-03-15 10:01:49 +0100

    Update i3.sh

--- i3.sh
+++ i3.sh
@@ -4,7 +4,7 @@
 sudo pacman -Syyu xorg lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings i3 i3-gaps i3lock i3status materia-gtk-theme papirus-icon-theme xfce4-terminal  --noconfirm --needed
 
 #install optional packages
-sudo pacman -S ibus okular gparted xf86-input-synaptics ffmpegthumbs nemo ttf-dejavu ttf-liberation dmenu nitrogen picom lxappearance archlinux-wallparper --noconfirm --needed
+sudo pacman -S ibus okular gparted xf86-input-synaptics ffmpegthumbs nemo ttf-dejavu ttf-liberation dmenu nitrogen picom lxappearance archlinux-wallpaper --noconfirm --needed
 
 #enable sddm
 sudo systemctl enable lightdm
