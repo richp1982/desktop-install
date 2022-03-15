ae5eaa431b6bf33d5791941d09202541d250d535
richp1982 <100537747+richp1982@users.noreply.github.com>
2022-03-15 08:19:04 +0100

    Update desktop-install.sh

--- desktop-install.sh
+++ desktop-install.sh
@@ -5,22 +5,22 @@
 #sudo chmod +x i3.sh && ./i3.sh
 
 #install audio & DVD packages
-sudo pacman -S alsa-utils alsa-firmware pulseaudio pulseaudio-alsa pulseaudio-equalizer pulseaudio-jack vlc mplayer rhytmbox audiocd-kio libcdio lidvdread libdvdcss libdvdnav --noconfirm --needed 
+sudo pacman -S alsa-utils alsa-firmware pulseaudio pulseaudio-alsa pulseaudio-equalizer pulseaudio-jack vlc mplayer rhytmbox libcdio lidvdread libdvdcss libdvdnav --noconfirm --needed 
 
 #enable bluetooth
 #sudo pacman -S bluez bluez-utils pulseaudio-bluetooth --no-confirm --needed && sudo systemctl start bluetooth.service
 
 #Install apps
-sudo pacman -S gimp blender libreoffice-fresh libreoffice-fresh-en-gb libreoffice-fresh-fr libreoffice-fresh-pl handbrake kdenlive 
+#sudo pacman -S gimp blender libreoffice-fresh libreoffice-fresh-en-gb libreoffice-fresh-fr libreoffice-fresh-pl handbrake kdenlive 
 
 #set up printer
-sudo chmod +x printer-setup.sh && sudo ./printer-setup.sh
+#sudo chmod +x printer-setup.sh && sudo ./printer-setup.sh
 
 #add users
-sudo chmod +x setup-users.sh && sudo ./setup-users.sh
+#sudo chmod +x setup-users.sh && sudo ./setup-users.sh
 
 #set up home directories
-sudo chmod +x home-directories.sh && sudo ./home-directories.sh
+#sudo chmod +x home-directories.sh && sudo ./home-directories.sh
 
 #run clean-up
 cd /a/kde-setup
