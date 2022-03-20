3f0b12af33682ee1d062f1cc1d076715b065dbb3
richp1982 <100537747+richp1982@users.noreply.github.com>
2022-03-20 21:00:46 +0100

    Update i3.sh

--- i3.sh
+++ i3.sh
@@ -1,15 +1,29 @@
 #!/bin/sh
 
 #install desktop packages
-sudo pacman -Syyu xorg lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings i3 i3-gaps i3lock i3status materia-gtk-theme papirus-icon-theme xfce4-terminal  --noconfirm --needed
+sudo pacman -Syyu xorg lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings i3 i3-gaps i3lock materia-gtk-theme papirus-icon-theme xfce4-terminal  --noconfirm --needed
 
 #install optional packages
-sudo pacman -S ibus okular gparted xf86-input-synaptics ffmpegthumbs nemo ttf-dejavu ttf-liberation dmenu nitrogen picom lxappearance archlinux-wallpaper --noconfirm --needed
+sudo pacman -S ibus okular gparted xf86-input-synaptics ffmpegthumbs nemo ttf-dejavu ttf-liberation feh picom lxappearance archlinux-wallpaper polybar --noconfirm --needed
 
 #enable sddm
 sudo systemctl enable lightdm
 
+#install rofi
+cd /a
+sudo pacman -S rofi --noconfirm --needed
+git clone --depth=1 https://github.com/adi1090x/rofi.git
+cd rofi
+chmod +x setup.sh
+sudo ./setup.sh
+
 #enable numlock on boot
 sudo pacman -S numlockx
 echo "greeter-setup-script=/usr/bin/numlockx on" | tee -a /etc/lightdm/lightdm.conf
+
+#Copy .files
+cd /a/desktop-install
+cp config /home/rich/.config/i3/
+mkdir /home/rich/.config/polybar
+cp config.ini /home/rich/.config/polybar/
 
