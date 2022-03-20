a8282114aad2f445f021188dc22525d3b821a745
richp1982 <100537747+richp1982@users.noreply.github.com>
2022-03-20 21:18:04 +0100

    Update i3.sh

--- i3.sh
+++ i3.sh
@@ -2,9 +2,10 @@
 
 #install desktop packages
 sudo pacman -Syyu xorg lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings i3 i3-gaps i3lock materia-gtk-theme papirus-icon-theme xfce4-terminal  --noconfirm --needed
+yay -S polybar
 
 #install optional packages
-sudo pacman -S ibus okular gparted xf86-input-synaptics ffmpegthumbs nemo ttf-dejavu ttf-liberation feh picom lxappearance archlinux-wallpaper polybar --noconfirm --needed
+sudo pacman -S ibus okular gparted xf86-input-synaptics ffmpegthumbs nemo ttf-dejavu ttf-liberation feh picom lxappearance archlinux-wallpaper --noconfirm --needed
 
 #enable sddm
 sudo systemctl enable lightdm
@@ -26,4 +27,6 @@ cd /a/desktop-install
 cp config /home/rich/.config/i3/
 mkdir /home/rich/.config/polybar
 cp config.ini /home/rich/.config/polybar/
+chmod +x launch.sh
+cp launch.sh /home/rich/.config/polybar/
 
