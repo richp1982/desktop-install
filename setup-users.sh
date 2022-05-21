356d206512edf3e9b95ce73702eb0f1157e412d1
richp1982 <100537747+richp1982@users.noreply.github.com>
2022-05-21 12:02:27 +0200

    Update setup-users.sh

--- setup-users.sh
+++ setup-users.sh
@@ -1,13 +1,23 @@
 #!/bin/sh
 
-#set up additional users arch
+#set up additional users
 
 #create new users
-sudo useradd -m -G sys,floppy,scanner,users,video,storage,optical,lp,audio,adm edyta
-sudo useradd -m -G sys,floppy,scanner,users,video,storage,optical,lp,audio,adm kiwi
+sudo useradd -G scanner,users,video,storage,optical,lp,audio edyta
+sudo useradd -G scanner,users,video,storage,optical,lp,audio kiwi
 
 #set passwords
 echo 'enter password for edyta'
 sudo passwd edyta
 echo 'enter password for kiwi'
 sudo passwd kiwi
+
+sudo chown edyta:edyta /home/edyta
+sudo chown kiwi:kiwi /home/kiwi
+sudo chown rich:users /data
+sudo chmod g+rwxs /data
+sudo chown -R rich:users /data/shared
+sudo chmod g+rwxs /data/shared
+sudo chown rich:rich /data/users/rich
+sudo chown kiwi:kiwi /data/users/kiwi
+sudo chown edyta:edyta /data/users/edyta
